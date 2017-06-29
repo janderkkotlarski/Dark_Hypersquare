c66c2e906af8739598eb187bb8f1aaaa515398d2
janderkkotlarski <j.d.kotlarski@gmx.com>
2017-06-29 16:50:43 +0200

    Further implement own integer vectors

--- wektor.cpp
+++ wektor.cpp
@@ -1,1 +1,25 @@
 #include "wektor.h"
+
+wektor::wektor(const int x,
+               const int y)
+    : m_x(x),
+      m_y(y)
+{ }
+
+wektor::wektor(wektor &wek)
+    : m_x(wek.get_x()),
+      m_y(wek.get_y())
+{ }
+
+wektor::~wektor()
+{ }
+
+wektor wektor::operator+ (wektor &wek) noexcept
+{
+    const int x_temp
+    { wek.get_x() };
+
+    wektor wek_temp(x_temp,
+                    x_temp);
+    return wek_temp;
+}
--- wektor.h
+++ wektor.h
@@ -1,9 +1,31 @@
 #ifndef WEKTOR_H
 #define WEKTOR_H
 
+#include <cassert>
+
 class wektor
 {
+    int m_x;
+    int m_y;
 
+public:
+
+    wektor(const int x,
+           const int y);
+
+    wektor(wektor &wek);
+
+    ~wektor();
+
+    int get_x() noexcept
+    { return m_x; }
+
+    int get_y() noexcept
+    { return m_y; }
+
+    wektor operator+ (wektor &wek) noexcept;
+
+    wektor operator- (wektor &wek) noexcept;
 };
 
 #endif // WEKTOR_H
