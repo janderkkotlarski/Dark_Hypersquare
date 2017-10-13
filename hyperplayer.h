259502ebf7873a56a84606d3ce0a79fabecad178
janderkkotlarski <j.d.kotlarski@gmx.com>
2017-10-13 12:47:22 +0200

    update

--- hyperplayer.h
+++ hyperplayer.h
@@ -11,6 +11,9 @@ class hyperplayer
 {
     std::vector <int> m_posit;
     std::vector <int> m_forward;
+    std::vector <int> m_rightward;
+    std::vector <int> m_backward;
+    std::vector <int> m_leftward;
     hypercolor m_color;
     hypersquare m_innersquare;
 
@@ -33,6 +36,19 @@ public:
 
     void move_forward()
     { add_direct(m_posit, m_forward); }
+
+    void move_rightward()
+    { add_direct(m_posit, m_rightward); }
+
+    void move_backward()
+    { add_direct(m_posit, m_backward); }
+
+    void move_leftward()
+    { add_direct(m_posit, m_leftward); }
+
+    void turn_right();
+
+    void turn_left();
 
 };
 
